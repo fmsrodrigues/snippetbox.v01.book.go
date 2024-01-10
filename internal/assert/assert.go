a4a0232e40e824d4e7e97a354a340199a7e8712e
Phobia <filipe_msr@hotmail.com>
2024-01-10 19:58:43 -0300

    Add integration and HTML tests and code coverage

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -1,11 +1,30 @@
 package assert
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func Equal[T comparable](t *testing.T, actual, expected T) {
 	t.Helper()
 
 	if actual != expected {
 		t.Errorf("expected %v; got %v", expected, actual)
+	}
+}
+
+func StringContains(t *testing.T, actual, expectedSubsting string) {
+	t.Helper()
+
+	if !strings.Contains(actual, expectedSubsting) {
+		t.Errorf("expected %q to contain %q", actual, expectedSubsting)
+	}
+}
+
+func NilError(t *testing.T, actual error) {
+	t.Helper()
+
+	if actual != nil {
+		t.Errorf("expected nil error; got %v", actual)
 	}
 }
